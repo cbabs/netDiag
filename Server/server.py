62575e3f75b1be07594752acba3fe6ba7d1f4d2f
Chris Babcock <chris@bluegreenit.com>
2019-06-14 07:19:29 -0500

    Changed server code to work with python client as opposed to batch file

--- Server/server.py
+++ Server/server.py
@@ -2,103 +2,46 @@ import os
 import time
 import datetime
 import json
-from pymongo import MongoClient
+from mongodb import MongoDb
 import subprocess
 import jsoncomment
 from json.decoder import JSONDecodeError
+from dateutil import parser
+import re
 
 
 
 class NetDiag(object):
 
-    def __init__(self, fileShare, dbHost=None, dbPort=None, dbUser=None, dbPass=None, dbName=None):
+    def __init__(self):
 
-        dbClient = MongoClient(dbHost, dbPort,
-                               username=dbUser, password=dbPass)
-        self.db = dbClient[dbName]
-        self.fileShare = fileShare
+        db = MongoDb()
+        self.db = db
 
 
-    # Read files on file share.  If not json, erase.
-    def getData(self):
-        
-        dirLst = os.listdir(self.fileShare) # List of file in dir
-        
-        # Loop over files in dir
-        for file in dirLst:
-    
-            
-    
-            # If file ends with .json process
-            if file.endswith(".json"):
-                
-                rmArg = "{}/{}".format(self.fileShare, file)
-                
-                ## This block removes last line of JSON file.
-                ## Needed for JSON lib to read
-                
-                fRemLast = open(file, "r+", encoding = "utf-8")
-
-                #Move the pointer to the end of the file. 
-                fRemLast.seek(0, os.SEEK_END)
-                
-                # Last char in file
-                pos = fRemLast.tell() - 1
-                
-                #Read each character backwrds until new line
-                while pos > 0 and fRemLast.read(1) != "\n":
-                    pos -= 1
-                    fRemLast.seek(pos, os.SEEK_SET)
-                
-                # Delete all the chars ahead of this position if ! beginning
-                if pos > 0:
-                    fRemLast.seek(pos, os.SEEK_SET)
-                    fRemLast.truncate()
-                
-                fRemLast.close() # Close the file
-                
-                
-                # Open file for JSON processing
-                with open(file, "r+", encoding='utf-8') as f:
-
-                    fRead = f.read() # Read file into string
-
-                    # Keep line breaks in json wrapper(see jsonComment docs)
-                    fRead2 = fRead.replace("\n","\n\\n")
-
-                    json = JsonComment() # Call JSON wrapper                    
-                    jsonData = json.loads(fRead2) # Convert to JSON
-
-                    dbData = self.processData(jsonData)
-                    
-                    f.close()
-                    
-                    
-
-                    
-                print(rmArg)
-                #subprocess.run(["rm", rmArg]) # Remove the file
-            
-    
-            #self.processData(jsonData) # Process raw JSON data
-    
     def processData(self, jsonData):
-        print(jsonData)
-        
+
         transDict = None
 
         tStamps = self.getTimeStamps()
 
-        ip = jsonData["ip"]
+        for k, v in jsonData.items():
+            print('{}: {}'.format(k, k))
+
+        jsonData = jsonData["netData"]
+
+        ip = jsonData["ipAdd"]
         print(type(ip))
 
         # Dict for transaction record
         diagRecord = {
             "dateSrvImpt": tStamps["timeStmp"],
             "epochSrvImpt": tStamps["epoch"],
-            "dateUserRan": jsonData["runTmStmp"].replace("\n", ""),
-            "userId": jsonData["userId"].replace("\n", ""),
-            "ticketNum": jsonData["ticketNum"].replace("\n", ""),
+            "userName": jsonData["userName"],
+            "hostName": jsonData["hostName"],
+            "dateUserRan": parser.parse(jsonData["dateUserRan"]),
+            "userId": jsonData["userName"],
+            "ticketNum": jsonData["ticketNum"],
             "ipconfig": self.fltrIpInfo(ip),
             "traceGoglDns": self.fltrTracRt(jsonData["trcRtPubDns"]),
             "traceSdcDns": self.fltrTracRt(jsonData["trcRtSdcDns"]),
@@ -109,23 +52,15 @@ class NetDiag(object):
             "pingNdclDns": self.fltrPing(jsonData["pgNdcDns"]),
             "pingTnGov": self.fltrPing(jsonData["pgTnGov"])
             }
-        
+
         self.addTransac(diagRecord)
 
     # Create a transaction
     def addTransac(self, transDict):
-        
-        print(transDict)
-        return
-        
-        # I should put a comment here
-        lastTransId = self.getLastRecord("transactions")
-        nextTransId = lastTransId + 1
 
         # Add doc to collections
-        record = self.db.transactions.insert_one(transDict).inserted_id
-        
-        
+        record = self.db.addTransac(transDict)
+
         return record["transId"]
 
     # Gets dict of current time stamps
@@ -140,127 +75,127 @@ class NetDiag(object):
         retrnDict = {"epoch": ts, "timeStmp": st}
 
         return retrnDict
-    
+
     # Process and return ipconfig data
     def fltrIpInfo(self, data):
 
         ipLines = data.splitlines() # Create array from data
-        
+
         lstLength = len(ipLines) # Get list
-        
+
         # Regex
         confKeyRgx = r'^(.*?)(\s\.)'
         confValRgx = r'(:\s)(.*)'
-        
+
         retrnDict = [] # Dict to return data from func
-        
+
         lstCnt = 0 # Index for loop count
         subConfDict = {}
         for line in ipLines:
-            
+
             lstCnt += 1 # Increment loop index
-            
+
             # If it last itertion append data end exit loop
             if lstCnt == lstLength:
                 print("Last one")
                 retrnDict.append(subConfDict)
                 break
-            
+
             # Skip line if empty
             if not line:
                 continue
-            
+
             # If line 1st char is a capital letter, create new dict
             if re.match(r'^[A-Z]', line):
-                
+
                 if subConfDict:
                     retrnDict.append(subConfDict)
-                
+
                 subConfDict = {line: {}}
                 confSect = line
-                
+
                 continue
-            
+
             # Get regex values from line
             confKey = re.match(confKeyRgx, line)
             confVal = re.search(confValRgx, line)
-        
+
             # Get key without leading white space
-            if confKey: 
+            if confKey:
                 confKey = confKey.group(1).strip()
             # Get value
             if confVal:
                 confVal = confVal.group(2)
-            
+
                 # Process dns servers to return list as value, up to 3 servers
                 if "DNS Servers" in confKey:
                     dnsValLst = []
-                    
+
                     # Append first dns server
                     dnsValLst.append(confVal)
                     nextDnsSrv = lstCnt # lstCnt += 1  above. Set for next line
-                    
+
                     # If line has capital letter(not ip addr), do nothing
                     if re.search(r'[A-Z]', ipLines[nextDnsSrv]) is None:
                         dnsValLst.append(ipLines[nextDnsSrv].strip())
-                    
+
                     nextDnsSrv += 1 # Check next server
                     # If line has capital letter(not ip addr), do nothing
                     if re.search(r'[A-Z]', ipLines[nextDnsSrv]) is None:
                         dnsValLst.append(ipLines[nextDnsSrv].strip())
                         subConfDict[confSect][confKey] = dnsValLst
-                        
+
                         continue
-    
+
                     subConfDict[confSect][confKey] = dnsValLst
                     continue
-                    
-            
+
+
             # If line empty, do nothing and exit loop
             if confKey is None: continue
-            
+
             # Add kv to dict
             subConfDict[confSect][confKey] = confVal
-            
+
         return retrnDict
-    
+
     # Process process info into list of dicts
     def fltrTracRt(self, data):
-    
+
         # Regex
         regxDigts = r'[0-9*\.]+'
         regxWords = r'[a-zA-Z0-9_\.,\[\]]+'
 
-    
+
         data = data.splitlines() # Create list from input data
-    
+
         retrnDict = {"ipAddr": "", "traceList": []} # List to be returned from func
-        
+
         regxIpAddr = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' # Sloppy ip addr search
-        
+
         for line in data:
-            
+
             print(line)
-            
+
             if not line: continue # If line is empty, continue loop
-            
+
             # Ignore lines with Trace complete and empty ones
             if "Trace complete" in line: continue
             if re.search(r'[a-z]', line) is None: continue
-            
+
             wrdsLst = re.findall(regxWords, line) # List from words in line
             digtLst = re.findall(regxDigts, line) # List from words in line
 
-            
+
             if "Tracing" in line:
                 ipAddr = re.search(regxIpAddr, line)
                 print(ipAddr[0])
                 retrnDict["ipAddr"] = ipAddr[0]
                 continue
-            
-               
+
+
             if "over a" in line: continue
-    
+
             # Dict to append to list
             lineDict = {
                 "tracOrdrNum": digtLst[0],
@@ -268,30 +203,30 @@ class NetDiag(object):
                 "trip2time": digtLst[2],
                 "trip3time": digtLst[3],
                 "ipHop": digtLst[4]
-                }       
-        
+                }
+
             retrnDict["traceList"].append(lineDict) # Append list to be returned
-                
+
         return retrnDict
-    
-    
+
+
     # Process process info into list of dicts
     def fltrProcInfo(self, data):
-    
+
         # Regex
         regxWords = '[a-zA-Z0-9_\.,]+'
-    
+
         data = data["topAppMem"].splitlines() # Create list from input data
-    
+
         retrnLst = [] # List to be returned from func
-        
+
         for line in data:
-            
+
             # Only check lines with 'K' at end
             if re.search(r'K$', line):
-    
+
                 wrdsLst = re.findall(regxWords, line) # List from words in line
-            
+
                 # If process is Memory Compression, combine words into var
                 if "Memory" in wrdsLst[0]:
                     procName = "{} {}".format(wrdsLst[0], wrdsLst[1])
@@ -299,62 +234,62 @@ class NetDiag(object):
                 else:
                     procName = wrdsLst[0]
                     procId = wrdsLst[1]
-                
-                # Create dict to append to return list 
+
+                # Create dict to append to return list
                 lineDict = {
                     "proc": procName,
                     "procId": procId,
                     "session": wrdsLst[2],
                     "memory": int(wrdsLst[4].replace(",",""))
                     }
-                
+
                 print(lineDict)
-                
+
                 retrnLst.append(lineDict) # Append list to be returned
-                
+
         return retrnLst
-    
-    
+
+
     def fltrPing(self, data):
-    
+
         # Regex
         regxDigits = r'[0-9]+' # Find digit groups
         regxIpAddr = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' # Sloppy ip search
-    
+
         data = data.splitlines() # Create list from input data
-    
+
         retrnDict = {} # List to be returned from func
-    
+
         for line in data:
-            
-    
+
+
             if not line: continue # If line is empty, continue loop
-            
+
             # Ignore empty lines
             if re.search(r'[a-z]', line) is None: continue
-            
+
             if "Pinging" in line:
                 ipAddr = re.search(regxIpAddr, line)
                 retrnDict["ipAddr"] = ipAddr[0]
                 continue
-    
+
             if "Packets" in line:
                 packtLoss = re.findall(r'[0-9]+', line)
                 retrnDict["pingLossPrct"] = packtLoss[3]
                 continue
-    
+
             if "Minimum" in line:
                 lineWords = re.findall(regxDigits, line)
                 retrnDict["latncyMin"] = lineWords[0]
                 retrnDict["latncyMax"] = lineWords[1]
                 retrnDict["latncyAvg"] = lineWords[2]
-                continue      
-                
+                continue
+
         return retrnDict # Return dict
 
 
 def main():
-    
+
     pass
 
 if __name__ == "__main__":
--- Server/views.py
+++ Server/views.py
@@ -10,58 +10,82 @@ from functools import wraps
 
 from mongodb import MongoDb
 
+from server2 import NetDiag
+
 db = MongoDb()
 
 bootstrap = Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def transactions():
-    
+
     if request.method == 'GET':
         webData = db.getAllTrans()
-    
+
         return render_template('transactions.html', data=webData)
 
     if request.method == 'POST':
-        
+
         if 'dateRange' in request.form:
             dates = request.form['dateRange']
-            
+
             regxBegDate = r'^(.*)(\s-\s)'
             regxEndDate = '(\s-\s)(.*)'
-            
+
             begDateRgx = re.findall(regxBegDate, dates)
             begDate = datetime.strptime(begDateRgx[0][0], '%Y-%m-%d %H:%M:%S')
-            
+
             endDateRgx = re.findall(regxEndDate, dates)
             endDate = datetime.strptime(endDateRgx[0][1], '%Y-%m-%d %H:%M:%S')
-                        
+
             webData = db.getTransDates(begDate, endDate)
-            
-            
+
+
             return render_template('transactions.html', data=webData)
 
         if 'ticketNum' in request.form:
             ticketNum = request.form['ticketNum']
-            
-            webData = db.getTransTckNum(ticketNum)       
-        
+
+            webData = db.getTransTckNum(ticketNum)
+
             return render_template('transactions.html', data=webData)
-    
+
         if 'transNum' in request.form:
             transNum = request.form['transNum']
             webData = []
             webDataDict = db.getTransac(transNum)
             webData.append(webDataDict)
-            
-            
+
             return render_template('transactions.html', data=webData)
-        
-# Get transaction by number       
+
+# Get transaction by number
 @app.route('/transacs/<slug>', methods=['GET'])
 def transacs(slug):
 
     slug = int(slug)  # Convert slug into integer
     data = db.getTransac(slug)  # Get transaction by id
 
+
+
+    #data["dateUserRan"] = data["dateUserRan"].strftime("%H:%M:%S.%f - %b %d %Y")
+    #data["dateSrvImpt"] = data["dateSrvImpt"].strftime("%H:%M:%S.%f - %b %d %Y")
+
+    print("\n\n\n")
+    print(data)
+    print("\n\n\n")
+
+
+
+
     return render_template('transacs.html', data=data)
+
+@app.route('/_api/upload-diag', methods=['POST'])
+def deleteFilter():
+
+    data = request.json
+
+    ndiag = NetDiag()
+
+    ndiag.processData(data)
+
+    return jsonify(result=data)
