0154143f7b885a501120e2725478c4dfc3c005d7
Chris Babcock <chris@bluegreenit.com>
2019-09-26 09:11:58 -0500

    Added Jinja filters and related functions.  Also enabled jinja loop controls ext

--- Server/views.py
+++ Server/views.py
@@ -3,6 +3,8 @@ from Server import app
 from flask import Flask, request, render_template, url_for, jsonify, redirect, flash, session
 from flask_bootstrap import Bootstrap
 
+from jinja2.ext import Extension
+
 import re
 from datetime import datetime
 
@@ -13,6 +15,20 @@ from mongodb import MongoDb
 from server import NetDiag
 
 db = MongoDb()
+
+
+def is_list(value):
+    return isinstance(value, list)
+
+def is_dict(value):
+    return isinstance(value, dict)
+
+app.jinja_env.filters.update({
+        'is_list': is_list,
+        'is_dict': is_dict,
+    })
+
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
 bootstrap = Bootstrap(app)
 
@@ -28,6 +44,9 @@ def transactions():
 
         if 'dateRange' in request.form:
             dates = request.form['dateRange']
+            
+            print(f"Data: {dates}")
+            print(type(dates))
 
             regxBegDate = r'^(.*)(\s-\s)'
             regxEndDate = '(\s-\s)(.*)'
@@ -37,9 +56,12 @@ def transactions():
 
             endDateRgx = re.findall(regxEndDate, dates)
             endDate = datetime.strptime(endDateRgx[0][1], '%Y-%m-%d %H:%M:%S')
+            
+            print(f"data to getTransDates {begDate} - {endDate}")
 
             webData = db.getTransDates(begDate, endDate)
 
+            print(f"From views.py: {webData}")
 
             return render_template('transactions.html', data=webData)
 
