626a541c214b957b19ff65fcd5298f7aeba82e62
Chris Babcock <chris@bluegreenit.com>
2020-11-25 08:26:01 -0600

    Added mutex to unregiester func and disabled comp

--- Server/wsServer.py
+++ Server/wsServer.py
@@ -23,30 +23,30 @@ class wsServer(object):
         self.USERS = set()
         self.USERS_MAPPING = []
 
-        self.start_server = websockets.serve(self.srvHandler, 'localhost', portNum)
+        self.start_server = websockets.serve(self.srvHandler, 'localhost',
+                                             portNum, compression=None)
+
+        self.lock = asyncio.Lock()
 
         asyncio.ensure_future(self.console_input())
         asyncio.get_event_loop().run_until_complete(self.start_server)
         asyncio.get_event_loop().run_forever()
 
 
-
     async def register(self, hostname, websocket):
-        self.USERS.add(websocket)
         self.USERS_MAPPING.append([hostname, websocket])
 
 
     async def unregister(self, websocket):
-        self.USERS.remove(websocket)
 
-        
-        userMappingListCopy = self.USERS_MAPPING
+        # Lock list to prevent race condition
+        async with self.lock: 
+            userMappingListCopy = self.USERS_MAPPING
 
-        for inx, val in enumerate(userMappingListCopy):
+            for inx, val in enumerate(userMappingListCopy):
 
-            if val[1] == websocket:
-                self.USERS_MAPPING.pop(inx)
-
+                if val[1] == websocket:
+                    self.USERS_MAPPING.pop(inx)
 
 
     async def processClientHostname(self, recvData):
@@ -63,8 +63,7 @@ class wsServer(object):
                 return item[1]
 
 
-
-    async def sendAndGetCmds(self, hostname, cmdsToExecute):
+    async def sendCmdsToClient(self, hostname, cmdsToExecute):
         websocket = await self.getWsObjectByHostname(hostname)
 
         if "," in cmdsToExecute:
@@ -76,19 +75,18 @@ class wsServer(object):
         strOfCmds = f'{{"remExecCmds": {cmdList}}}'
         print(strOfCmds)
         await websocket.send(strOfCmds)
-        cmdRecv = await websocket.recv()
-        print(cmdRecv)
 
 
     async def console_input(self):
         hostname = await ainput('Enter hostname: ')
         exeCmd = await ainput('Enter cmd: ')
-        await self.sendAndGetCmds(hostname, exeCmd)
+        await self.sendCmdsToClient(hostname, exeCmd)
+
 
     async def consumer_handler(websocket, path):
         async for message in websocket:
             await consumer(message)
-            
+
             
     async def producer_handler(websocket, path):
         while True:
@@ -96,10 +94,8 @@ class wsServer(object):
             await websocket.send(message)
 
 
-    async def srvHandler(self, websocket, path):
-        # register(websocket) sends user_event() to websocket
-
-
+    async def registerClient(self, websocket):
+        
         msgRecv = await websocket.recv() # Get hostname sent from client
         clntHostname = await self.processClientHostname(msgRecv)
 
@@ -107,19 +103,29 @@ class wsServer(object):
         print(self.USERS_MAPPING)
 
 
+    async def srvHandler(self, websocket, path):
+        # register(websocket) sends user_event() to websocket
+
+        await self.registerClient(websocket)
+
         try:
             async for message in websocket:
-                data = json.loads(message)
-                if data["action"] == "minus":
-                    STATE["value"] -= 1
-                    await notify_state()
-                elif data["action"] == "plus":
-                    STATE["value"] += 1
-                    await notify_state()
-                else:
-                    logging.error("unsupported event: {}", data)
+                
+                print("handler loop " + str(message))
+                
+                #TODO add function to send to rabbitmq 
+
         finally:
             await self.unregister(websocket)
+
+
+def main():
+
+    wss = wsServer()
+    wss()
+
+if __name__ == "__main__":
+    main()
 
 
 
@@ -161,18 +167,3 @@ class wsServer(object):
             time.sleep(5000)
             num += 1
     '''
-
-
-def main():
-
-    wss = wsServer()
-
-
-
-    wss()
-
-    wss.sendCmd("DESKTOP-RGFH0PI", "whoami")
-
-
-if __name__ == "__main__":
-    main()
