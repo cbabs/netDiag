ef692e0ae4f7b00dc2608bd0864816e6fda78a4d
Chris Babcock <chris@bluegreenit.com>
2020-12-02 08:27:22 -0600

    Removed old debug code

--- client/wsClient.py
+++ client/wsClient.py
@@ -19,28 +19,28 @@ def runOsCmd(osCmd):
 
 async def startClient():
     async with websockets.connect(
-            'ws://localhost:8765') as websocket:
+            f'ws://10.100.0.113:8765') as websocket:
         hostName = runOsCmd("hostname")
-        hostName = hostName.decode('UTF-8').strip().replace('\r\n', '\n') + "+" + str(random.randint(1, 50))
+        hostName = hostName.decode('UTF-8').strip().replace('\r\n', '\n')
         print(hostName)
 
         msgSend = {"regstrMchine": hostName}
         msgSend = json.dumps(msgSend)
 
-        await websocket.send(msgSend)      
+        await websocket.send(msgSend)
 
         #TODO Make own func
         async for message in websocket:
-            
+
             print("handler loop " + str(message))
             print(message)
             msgRecv = message
-            print(f"in  {msgRecv}")
+            print(f"in < {msgRecv}")
             #   await processWsMesg(msgRecv, websocket, hostName)
 
 
 #async def processWsMesg(msgRecv, websocket, hostName):
-            if "run_report" in msgRecv:    
+            if "run_report" in msgRecv:
                 try:
                     print(subprocess.run("client", capture_output=True))
                 except Exception as e:
@@ -63,18 +63,17 @@ async def startClient():
                     except Exception as e:
                         print(e)
                         cmdReturn = e
-                    
+
                     msgToSend = f'{{"{hostName}":{cmdReturn}}}'
                     print(f"out > {msgToSend}")
                     await websocket.send(msgToSend)
-            
-
-
 
 
 def main():
+    print(__name__)
     asyncio.get_event_loop().run_until_complete(startClient())
     asyncio.get_event_loop().run_forever()
+
 
 if __name__ == "__main__":
     main()
